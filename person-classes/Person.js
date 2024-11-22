57b3ffe77eb2e757dbf41072a5755ccdd4f91f38
Siyabonga Samson Lukhele <samsonlukhele76@gmail.com>
2024-11-22 15:26:35 +0200

    Fix pronouns

--- person-classes/Person.js
+++ person-classes/Person.js
@@ -1,20 +1,21 @@
 class Person {
   constructor({ name, age, gender }) {
-    if (gender !== "male" && gender !== "female") {
-      throw new Error("Gender must be male or female");
-    }
     this.name = name;
     this.age = age;
     this.gender = gender;
+    this.prefix =
+      gender.toLowerCase() === "male"
+        ? "He"
+        : gender.toLowerCase() === "female"
+        ? "She"
+        : "They";
   }
 
   greet() {
     return `Hello, my name is ${this.name}.`;
   }
   getDetails() {
-    return `${this.name} is a ${this.age}-year-old ${
-      this.gender === "male" ? "boy" : "girl"
-    }.`;
+    return `${this.prefix} is ${this.age} years old.`;
   }
 }
 
