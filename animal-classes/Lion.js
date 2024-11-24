5f296bfbd309e0178306578cc93aad79d5014a3e
Siyabonga Samson Lukhele <samsonlukhele76@gmail.com>
2024-11-24 18:44:09 +0200

    Some more changes to the Lion class

--- animal-classes/Lion.js
+++ animal-classes/Lion.js
@@ -1,7 +1,23 @@
 const { Animal } = require("./Animal");
 
 class Lion extends Animal {
-  constructor({ name, age, species }) {
-    super({ name, age, species });
+  constructor({ name, age, species = "Lion", gender }) {
+    super({ name, age, species, gender });
+    const genderToLower = gender.toLowerCase();
+    if (genderToLower === "male" && age > 10) {
+      throw new Error("Male lions can not be older than 10 years");
+    }
+    if (genderToLower === "female" && age > 16) {
+      throw new Error("Female lions can not be older than 16 years");
+    }
+    if (genderToLower !== "male" && genderToLower !== "female") {
+      throw new Error("Invalid gender for Lion");
+    }
+  }
+  makeSound() {
+    return "Roar!";
+  }
+  hunt() {
+    return `${this.name} is hunting`;
   }
 }
